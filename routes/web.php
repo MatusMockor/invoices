f8424347fc84577cb5654aa9e175b3b7e45e802c
Matúš Močkor <33400567+MatusMockor@users.noreply.github.com>
2025-05-04 10:54:44 +0200

    pint

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,10 +18,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    
+
     // Invoice routes
     Route::resource('invoices', InvoiceController::class);
-    
+
     // Company routes
     Route::resource('companies', CompanyController::class);
     Route::post('/companies/fetch-by-ico', [CompanyController::class, 'fetchByIco'])->name('companies.fetch-by-ico');
