25715431f5bab47217252d0fe8d596765f345178
Matúš Močkor <33400567+MatusMockor@users.noreply.github.com>
2025-05-04 19:26:12 +0200

    feat(database): add PartnerFactory for test data generation
    Introduce a factory for the Partner model to streamline test data creation. This factory provides realistic mock data for partners, including related user associations and relevant business details.

---
+++ database/factories/PartnerFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Partner;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Partner>
+ */
+class PartnerFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Partner::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'name' => $this->faker->company(),
+            'ico' => $this->faker->numerify('########'),
+            'street' => $this->faker->streetAddress(),
+            'city' => $this->faker->city(),
+            'postal_code' => $this->faker->postcode(),
+            'country' => $this->faker->country(),
+            'dic' => $this->faker->numerify('##########'),
+            'ic_dph' => 'SK' . $this->faker->numerify('##########'),
+        ];
+    }
+}
