8cfd219013523bb09381e6e1db1e018bbee8caae
Matúš Močkor <33400567+MatusMockor@users.noreply.github.com>
2025-05-04 20:30:43 +0200

    feat(migrations): add current_company_id to users table
    Added a foreign key `current_company_id` to the users table, referencing the companies table. This field is nullable and will be set to null on deletion of the related company.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->foreignId('current_company_id')->nullable()->constrained('companies')->onDelete('set null');
             $table->rememberToken();
             $table->timestamps();
         });
