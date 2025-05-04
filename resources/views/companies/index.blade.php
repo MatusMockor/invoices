d88f23829ecb8b2667811bf708ac04a5c1f1316a
Matúš Močkor <33400567+MatusMockor@users.noreply.github.com>
2025-05-04 21:44:53 +0200

    refactor(views): simplify companies index actions layout
    Removed dropdown menus for company actions and replaced them with direct action buttons. This enhances the user interface by reducing complexity and improving accessibility.

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -93,33 +93,12 @@
                                                 </button>
                                             </form>
                                         @endif
-                                        <div class="group relative">
-                                            <button id="dropdown-button-{{ $company->id }}" aria-expanded="false" aria-haspopup="true" class="inline-flex items-center text-xs px-2.5 py-1.5 border border-gray-300 dark:border-gray-600 rounded bg-white dark:bg-gray-700 text-gray-700 dark:text-gray-300 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
-                                                    <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
-                                                </svg>
-                                            </button>
-                                            <div id="dropdown-menu-{{ $company->id }}" class="hidden absolute right-0 mt-2 w-40 rounded-md shadow-lg bg-white dark:bg-gray-800 ring-1 ring-black ring-opacity-5 z-10">
-                                                <div class="py-1" role="menu" aria-orientation="vertical" aria-labelledby="dropdown-button-{{ $company->id }}">
-                                                    <a href="{{ route('companies.edit', $company) }}" class="flex items-center px-4 py-2 text-sm text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700" role="menuitem">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2 text-gray-500" viewBox="0 0 20 20" fill="currentColor">
-                                                            <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
-                                                        </svg>
-                                                        {{ __('Edit') }}
-                                                    </a>
-                                                    <form method="POST" action="{{ route('companies.destroy', $company) }}" class="block w-full" onsubmit="return confirm('{{ __('Are you sure you want to delete this company?') }}');">
-                                                        @csrf
-                                                        @method('DELETE')
-                                                        <button type="submit" class="flex items-center w-full text-left px-4 py-2 text-sm text-red-600 dark:text-red-400 hover:bg-gray-100 dark:hover:bg-gray-700" role="menuitem">
-                                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2 text-red-500" viewBox="0 0 20 20" fill="currentColor">
-                                                                <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
-                                                            </svg>
-                                                            {{ __('Delete') }}
-                                                        </button>
-                                                    </form>
-                                                </div>
-                                            </div>
-                                        </div>
+                                                                                    <a href="{{ route('companies.edit', $company) }}" class="inline-flex items-center text-xs px-2.5 py-1.5 border border-gray-300 dark:border-gray-600 rounded bg-white dark:bg-gray-700 text-gray-700 dark:text-gray-300 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3 mr-1" viewBox="0 0 20 20" fill="currentColor">
+                                                <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                                            </svg>
+                                            {{ __('Edit') }}
+                                                                                    </a>
                                     </div>
                                 </div>
                             </div>
