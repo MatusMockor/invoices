06402e500e8d5911e986a33186166740ee71254a
Matúš Močkor <33400567+MatusMockor@users.noreply.github.com>
2025-05-04 13:22:39 +0200

    pint

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\API\CompanyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\CompanyController;
 
 /*
 |--------------------------------------------------------------------------
