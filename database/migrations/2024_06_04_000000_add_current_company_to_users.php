2e809afc04aaf64b0d2d9fb6f6279424d33a142b
Matúš Močkor <33400567+MatusMockor@users.noreply.github.com>
2025-05-05 20:11:41 +0200

    refactor(migrations): add static keyword to closures
    Updated anonymous functions to use the static keyword in the migrations file. This enhances performance and clarity by explicitly indicating that the closures do not bind to the current class instance.

--- database/migrations/2024_06_04_000000_add_current_company_to_users.php
+++ database/migrations/2024_06_04_000000_add_current_company_to_users.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('users', static function (Blueprint $table) {
             $table->foreignId('current_company_id')->nullable()->constrained('companies')->onDelete('set null');
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('users', static function (Blueprint $table) {
             $table->dropForeign(['current_company_id']);
             $table->dropColumn('current_company_id');
         });
