652c732f03f34ae9b71425d785099739745e497b
Matúš Močkor <33400567+MatusMockor@users.noreply.github.com>
2025-05-18 16:55:50 +0200

    **feat(views): update button text color to black**
    Updated the text color of various buttons from white to black across multiple views for consistency and improved visual contrast. Dark mode styles remain unaffected, retaining a white text color.

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -49,7 +49,7 @@
                         @enderror
                     </div>
 
-                    <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">{{ __('Email Password Reset Link') }}</button>
+                    <button type="submit" class="w-full text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">{{ __('Email Password Reset Link') }}</button>
 
                     <p class="text-sm font-light text-gray-500 dark:text-gray-400">
                         <a href="{{ route('login') }}" class="font-medium text-primary-600 hover:underline dark:text-primary-500">{{ __('Back to login') }}</a>
