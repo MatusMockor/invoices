20852962e05f5a2ae27e94c26e5b15201e3fc758
Matúš Močkor <33400567+MatusMockor@users.noreply.github.com>
2025-05-18 18:49:15 +0200

    feat(BusinessEntityController): replace Partner with BusinessEntity
    Replaced all instances of PartnerResource and `partner_id` with BusinessEntityResource and `business_entity_id`. Added a new `BusinessEntityResource` class and updated dependencies, controller methods, database migration, and service bindings to reflect this change. This improves alignment with the updated domain model.

---
+++ app/Http/Resources/BusinessEntityResource.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Resources;
+
+use App\Models\BusinessEntity;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @mixin BusinessEntity
+ */
+class BusinessEntityResource extends JsonResource
+{
+    /**
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'status' => true,
+            'success' => true,
+            'data' => [
+                'ico' => $this->ico,
+                'name' => $this->name,
+                'street' => $this->street,
+                'city' => $this->city,
+                'postal_code' => $this->postal_code,
+                'country' => $this->country,
+                'dic' => $this->dic,
+                'ic_dph' => $this->ic_dph,
+                'company_type' => $this->company_type,
+                'registration_number' => $this->registration_number,
+            ],
+        ];
+    }
+}
