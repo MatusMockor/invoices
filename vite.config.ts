9d6805efcaa338bf605188581f89e15ec0064fea
Matúš Močkor <33400567+MatusMockor@users.noreply.github.com>
2025-10-28 12:07:26 +0100

    chore(workflows,vite): clean up config and adjust workflows
    Removed unused `extensions` configuration in `vite.config.ts` to streamline settings. Updated GitHub actions workflow to reposition the "Build frontend assets" step earlier, simplifying the process and ensuring proper environment setup.

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,6 @@ export default defineConfig({
         alias: {
             '@': path.resolve(__dirname, './resources/js'),
         },
-        extensions: ['.mjs', '.js', '.mts', '.ts', '.jsx', '.tsx', '.json'],
     },
     server: {
         host: '0.0.0.0',
