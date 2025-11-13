f34b68056d9ba48396edecf4ed7a3502a55a2235
Matúš Močkor <33400567+MatusMockor@users.noreply.github.com>
2025-11-13 18:36:13 +0100

    refactor(database): remove CompanyStatus enum from UserCompany
    Eliminated the use of the CompanyStatus enum in the UserCompany model and related files to simplify the structure and improve maintainability. Updated factories, models, and tests accordingly to reflect these changes.

--- database/factories/UserCompanyFactory.php
+++ database/factories/UserCompanyFactory.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Database\Factories;
 
-use App\Enums\CompanyStatus;
 use App\Models\UserCompany;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -46,7 +45,6 @@ class UserCompanyFactory extends Factory
             'website' => fake()->url(),
             'company_type' => fake()->randomElement($companyTypes),
             'registration_number' => 'OR '.fake()->randomElement(['Bratislava I', 'Košice', 'Žilina', 'Prešov', 'Banská Bystrica']).', Oddiel: '.fake()->randomElement(['Sro', 'Sa']).', Vložka č. '.fake()->numerify('######'),
-            'status' => fake()->randomElement([CompanyStatus::ACTIVE, CompanyStatus::INACTIVE]),
         ];
     }
 
